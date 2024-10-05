08b7c58a069c642038255e59097f3e61341aaa6c
GhostGamer <arambhmani@hotmail.com>
2024-10-05 22:00:05 +0530

    rotation option added

--- src/com/mime/minefront/graphics/Render3D.java
+++ src/com/mime/minefront/graphics/Render3D.java
@@ -11,22 +11,31 @@ public class Render3D extends Render {
 
 	public void floor(Game game) {
 		
+		double floorPosition = 8;
+		double ceilingPosition = 8;
+		double forward = game.time/5.0;
+		double right = game.time/5.0;
+		
+		double rotation = 0;
+		double cosine = Math.cos(rotation);
+		double sine = Math.sin(rotation);
+		
 		for (int y = 0; y < height; y++) {
 			double ceiling = (y - height / 2.0)/height;
 			
+			double z = floorPosition / ceiling;
 			if (ceiling < 0) {
-				ceiling = -ceiling;
+				z = ceilingPosition/ -ceiling;
 			}
 			
-			double z = 8 / ceiling;
 			
 			
 			for (int x = 0; x < width; x++) {
 
 				double depth = (x - width / 2.0)/height;
 				depth *= z;
-				double xx = depth;
-				double yy =z + game.time;
+				double xx = depth * cosine + z * sine + right;
+				double yy =z * cosine - depth * sine + forward;
 				int xPix = (int)xx;
 				int yPix = (int)yy;
 				pixels[x + y * width] = ((xPix &15)*16) | ((yPix &15)*16) << 8;
